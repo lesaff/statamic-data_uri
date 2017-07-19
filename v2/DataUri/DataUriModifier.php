20717a6eac601df43cbc0f7d0a52fa6a19bc6fcc
Rémy <git@rrelmy.ch>
2017-07-19 14:33:55 +0200

    fix code style issues / remove unused variable / add phpdoc

--- v2/DataUri/DataUriModifier.php
+++ v2/DataUri/DataUriModifier.php
@@ -9,9 +9,9 @@ class DataUriModifier extends Modifier
     /**
      * Modify a value
      *
-     * @param mixed  $value    The value to be modified
-     * @param array  $params   Any parameters used in the modifier
-     * @param array  $context  Contextual values
+     * @param mixed $value The value to be modified
+     * @param array $params Any parameters used in the modifier
+     * @param array $context Contextual values
      * @return mixed
      */
     public function index($value, $params, $context)
